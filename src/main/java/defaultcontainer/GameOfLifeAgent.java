48c86cee50e20760f417c07382f5f5ddd5817d93
Roodek <radmatyja@gmail.com>
2021-11-24 22:37:25 +0100

    game_of_life_v01

--- src/main/java/defaultcontainer/GameOfLifeAgent.java
+++ src/main/java/defaultcontainer/GameOfLifeAgent.java
@@ -3,13 +3,17 @@ package defaultcontainer;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameOfLifeAgent extends Agent {
 
-    private boolean isAlive;
+    private Boolean isAlive;
+    private Integer x,y,M,N,aliveNeighbours;
 
     /*_____________
     * |_X_|_X_|_X_|
@@ -24,51 +28,98 @@ public class GameOfLifeAgent extends Agent {
         System.out.println("agent: "+this.getAID()+" started");
         Object[] args = getArguments();
         this.isAlive = (boolean)args[0];
-        this.neighbourhood = (GameOfLifeAgent[])args[1];
+        this.x=(Integer)args[1];
+        this.y=(Integer)args[2];
+        this.M=(Integer)args[3];
+        this.N=(Integer)args[4];
 
-
+        try {
+            buildNeighbourhood();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         addBehaviour(new CyclicBehaviourUpdateState());
     }
 
-    private void buildNeighbourhood(){
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
-        otherAgents.add(new AID("",AID.ISLOCALNAME));
+    private void buildNeighbourhood() throws IOException {
+        if(isCellInsideGrid(x+1,y+1)) {
+            otherAgents.add(new AID("agent" + (x + 1) + (y + 1), AID.ISLOCALNAME));
+        }
+        if(isCellInsideGrid(x,y+1)){
+            otherAgents.add(new AID("agent"+(x)+(y+1),AID.ISLOCALNAME));
+        }
+        if(isCellInsideGrid(x+1,y)) {
+            otherAgents.add(new AID("agent" + (x + 1) + (y), AID.ISLOCALNAME));
+        }
+        if(isCellInsideGrid(x+1,y-1)) {
+            otherAgents.add(new AID("agent" + (x + 1) + (y - 1), AID.ISLOCALNAME));
+        }
+        if(isCellInsideGrid(x,y-1)){
+            otherAgents.add(new AID("agent"+(x)+(y-1),AID.ISLOCALNAME));
+        }
+        if(isCellInsideGrid(x-1,y-1)) {
+            otherAgents.add(new AID("agent" + (x - 1) + (y - 1), AID.ISLOCALNAME));
+        }
+        if(isCellInsideGrid(x-1,y)) {
+            otherAgents.add(new AID("agent" + (x - 1) + (y), AID.ISLOCALNAME));
+        }
+        if(isCellInsideGrid(x-1,y+1)){
+            otherAgents.add(new AID("agent"+(x-1)+(y+1),AID.ISLOCALNAME));
+        }
+
+    }
+
+    private boolean isCellInsideGrid(int x,int y){
+        if (x<0 || y<0 || x>M-1 || y>N-1)
+            return false;
+        else
+            return true;
+    }
+
+    private void informNeighbours() throws IOException {
+        ACLMessage aclmsg = new ACLMessage(ACLMessage.REQUEST);
+        for(AID i:otherAgents){
+            aclmsg.addReceiver(i);
+        }
+        aclmsg.setContentObject(this.isAlive);
+        send(aclmsg);
     }
 
     private class CyclicBehaviourUpdateState extends CyclicBehaviour {
         public void action() {
+            ACLMessage msg = null;
+            aliveNeighbours=0;
+            try {
+                informNeighbours();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for(int i=0; i<otherAgents.size(); i++){
+                msg = receive();
+
+                if(msg!=null){
+                    try {
+                        if((boolean)msg.getContentObject()){
+                            aliveNeighbours += 1;
+                        }
+                    } catch (UnreadableException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
             determineIfAliveOrDead();
         }
     }
 
-    private int countNeighbours(){
-        int aliveAgents =0;
-        for(GameOfLifeAgent agent:neighbourhood){
-            if(agent.getAliveState()){
-                aliveAgents+=1;
-            }
-        }
-        return aliveAgents;
-    }
-
     private void determineIfAliveOrDead(){
-        if(this.isAlive && (countNeighbours()==2 || countNeighbours()==3)){
+        if(this.isAlive && (aliveNeighbours==2 || aliveNeighbours==3)){
             return;
-        }else if(!isAlive && countNeighbours()==3){
+        }else if(!isAlive && aliveNeighbours==3){
             this.isAlive=true;
         }else{
             this.isAlive=false;
         }
     }
 
-    public boolean getAliveState(){
-        return isAlive;
-    }
 
 }
--- src/main/java/defaultcontainer/Main.java
+++ src/main/java/defaultcontainer/Main.java
@@ -7,11 +7,12 @@ import jade.util.ExtendedProperties;
 import jade.util.leap.Properties;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
 import jade.wrapper.StaleProxyException;
 
 public class Main {
 
-    public static void main(String[] args) throws StaleProxyException {
+    public static void main(String[] args) throws ControllerException {
 
         Runtime runtime = Runtime.instance();
 
@@ -35,10 +36,15 @@ public class Main {
         AgentController agentsSet;
         for(int i=0;i<M;i++){
             for(int j=0;j<N;j++){
-                agentsSet = mainContainer.createNewAgent("agent"+i+j, "defaultcontainer.GameOfLifeAgent", new Object[]{});
-                agentsSet.start();
+                mainContainer.createNewAgent("agent"+i+j, "defaultcontainer.GameOfLifeAgent", new Object[]{grid[i][j] == 1,i,j,M,N});
             }
         }
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                mainContainer.getAgent("agent"+i+j).start();
+            }
+        }
+
 
 
     }
